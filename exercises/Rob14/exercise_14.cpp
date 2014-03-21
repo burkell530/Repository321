16104577c30dfe1f6e72742084d691bd5b0d255e
burkell530 <burkell530@vm-0.burkell530.koding.kd.io>
2014-03-21 23:00:53 +0000

    Test setup for Laurie

---
+++ exercises/Rob14/exercise_14.cpp
@@ -0,0 +1,13 @@
+//
+// This is a test file for Lauri
+//
+
+#include <iostream>
+using namespace std;
+
+//
+// your assignment is to change this to say "Hello, world!"
+//
+int main() {
+    cout << "Hello" << endl;
+}
